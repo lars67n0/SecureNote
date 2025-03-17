16585f7dd107fdec369f06a3383bf57ffd3a4ea2
lars67n0 <lars67n0@edu.zealand.dk>
2025-03-17 12:14:02 +0100

    Added supabase/ssr

--- securenote/lib/supabase.ts
+++ securenote/lib/supabase.ts
@@ -1,11 +1,6 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr';
 
-export let supabase: SupabaseClient<any, "public", any>;
-if (typeof window !== 'undefined') {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-  if (supabaseUrl && supabaseAnonKey) {
-    supabase = createClient(supabaseUrl, supabaseAnonKey);
-  }
-}
+export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
